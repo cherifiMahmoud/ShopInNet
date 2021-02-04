01d03487c32714d77f64cc9d12b2ed69f0e50343
Cherifi Mahmoud <Cherifimahmoud97@gmail.com>
2021-02-04 09:23:26 +0100

    Add Categorie Model To Domain Project

---
+++ Domain/Models/Categorie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    class Categorie
+    {
+        [Key]
+        public int CategorieId { get; set; }
+        public string CategorieName { get; set; }
+    }
+}
